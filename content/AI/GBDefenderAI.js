eb9b8ade8564a7811624e3cf937328bda4bb94e5
Otamamori917 <117782096+Otamamori917@users.noreply.github.com>
2022-12-02 16:23:04 -0500

    hmm

---
+++ content/AI/GBDefenderAI.js
@@ -0,0 +1,20 @@
+const GBDefenderAI = prov(() => {
+  let u = extend(DefenderAI, { 
+    findTarget(air,ground){
+      let result = null;
+
+      if(air){
+        for(updateMovement){
+          set(boost = true)
+        }
+      }  
+      if(ground){
+        for(updateMovement){
+          set(boost = false)
+        }
+      }
+    }
+  }
+  )
+)    
+}
